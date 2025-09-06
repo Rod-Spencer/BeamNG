3a91ba535bd76b3a9ca160a650c53beb0dc1a4a9
Rod Spencer <rod.spencer.rs@gmail.com>
2025-09-06 17:43:19 -0500

    Updated Classification List to use "Classification Card"

--- DanBeamNG/Pages/ClassificationPages/Classification List.razor.cs
+++ DanBeamNG/Pages/ClassificationPages/Classification List.razor.cs
@@ -98,6 +98,34 @@ namespace SpenSoft.DanBeamNG.Pages.ClassificationPages
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////
         #region Public Methods
+
+        public async Task Delete_Classification_Handler(Classifications? cls)
+        {
+            if (cls == null) return;
+            Error = false;
+            try
+            {
+                if (classificationDataService != null)
+                {
+                    await classificationDataService.DeleteClassifications(cls.ID);
+                    var clsList = classificationDataService.GetAllClassifications();
+                    if (clsList != null)
+                    {
+                        classificationList = clsList?.Result?.OrderBy(x => x.Name).ToList();
+                    }
+
+                }
+                await Task.CompletedTask;
+            }
+            catch (Exception ex)
+            {
+                //logger.Error(Exception_Helper.FormatExceptionString(ex));
+                StatusClass = "alert-danger"; Message1 = $"The following Exception error was thrown while trying to delete the classification: {ex.Message}";
+                Message2 = "Please see runtime log for more details";
+                Error = true;
+            }
+        }
+
         #endregion
         ////////////////////////////////////////////////////////////////////////////////////////////////////
 
